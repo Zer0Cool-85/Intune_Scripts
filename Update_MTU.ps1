1c86deb94f4212d1c084cd0c1db87570382b2a4c
Dale <63794501+Zer0Cool-85@users.noreply.github.com>
2025-06-02 12:30:57 -0400

    Update Update_MTU.ps1

--- Update_MTU.ps1
+++ Update_MTU.ps1
@@ -1,4 +1,5 @@
-# MTU check and set script with logging
+# Silent MTU adjustment for physical adapters only (Ethernet, Wi-Fi, Dock)
+# Requires administrative privileges
 
 $logFile = "C:\Windows\Temp\MTU_Update_Log.txt"
 $desiredMTU = 1360
@@ -9,22 +10,28 @@ function Log {
     Add-Content -Path $logFile -Value "$timestamp - $message"
 }
 
-# Get all network adapters that are up
-$adapters = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
+# Define keywords for acceptable physical adapters
+$allowedKeywords = @("ethernet", "wi-fi", "wifi", "dock", "lan")
+
+# Get all network adapters (even if disabled)
+$adapters = Get-NetAdapter -IncludeHidden | Where-Object {
+    $_.HardwareInterface -eq $true -and
+    ($allowedKeywords | Where-Object { $_ -in $_.Name.ToLower() -or $_ -in $_.InterfaceDescription.ToLower() })
+}
 
 foreach ($adapter in $adapters) {
     $interfaceAlias = $adapter.Name
-    $ipInterface = Get-NetIPInterface -InterfaceAlias $interfaceAlias -AddressFamily IPv4
+    try {
+        $ipInterface = Get-NetIPInterface -InterfaceAlias $interfaceAlias -AddressFamily IPv4 -ErrorAction Stop
 
-    if ($ipInterface.NlMtu -ne $desiredMTU) {
-        Log "Changing MTU for '$interfaceAlias' from $($ipInterface.NlMtu) to $desiredMTU"
-        try {
+        if ($ipInterface.NlMtu -ne $desiredMTU) {
+            Log "Changing MTU for '$interfaceAlias' from $($ipInterface.NlMtu) to $desiredMTU"
             Set-NetIPInterface -InterfaceAlias $interfaceAlias -NlMtu $desiredMTU -AddressFamily IPv4 -ErrorAction Stop
             Log "Successfully updated MTU for '$interfaceAlias'"
-        } catch {
-            Log "ERROR: Failed to update MTU for '$interfaceAlias' - $_"
+        } else {
+            Log "No change needed. MTU for '$interfaceAlias' is already $desiredMTU"
         }
-    } else {
-        Log "No change needed. MTU for '$interfaceAlias' is already $desiredMTU"
+    } catch {
+        Log "ERROR: Could not process adapter '$interfaceAlias' - $_"
     }
 }
