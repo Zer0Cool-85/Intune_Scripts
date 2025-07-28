484be7a37287e489576456014a52af57afdaea38
Dale <63794501+Zer0Cool-85@users.noreply.github.com>
2025-07-28 10:33:39 -0400

    Update SwiftDialog.sh

--- SwiftDialog.sh
+++ SwiftDialog.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Define script variables
-logFile="/var/tmp/dialog.log"
+# ===== Define Variables =====
 DIALOG="/usr/local/bin/dialog"
-loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-loggedInUserFullName=$( id -F "${loggedInUser}" )
-loggedInUserFirstName=$( echo "$loggedInUserFullName" | sed -E 's/^.*, // ; s/([^ ]*).*/\1/' | sed 's/\(.\{25\}\).*/\1…/' | awk '{print ( $0 == toupper($0) ? toupper(substr($0,1,1))substr(tolower($0),2) : toupper(substr($0,1,1))substr($0,2) )}' )
+logFile="/var/tmp/dialog.log"
+loggedInUser=$(stat -f%Su /dev/console)
 userID=$(id -u "$loggedInUser")
 
-# Get the current hour (24-hour format)
+# ==== Greeting Logic ====
+loggedInUserFullName=$(id -F "${loggedInUser}")
+loggedInUserFirstName=$(echo "$loggedInUserFullName" | awk '{print $1}')
 currentHour=$(date +"%H")
 
-# Set greeting based on time
 if [ "$currentHour" -ge 5 ] && [ "$currentHour" -lt 12 ]; then
     timeGreeting="Good morning"
 elif [ "$currentHour" -ge 12 ] && [ "$currentHour" -lt 17 ]; then
@@ -20,11 +19,13 @@ else
     timeGreeting="Good evening"
 fi
 
+# ==== Prepare command file ====
 rm -f "$logFile"
 touch "$logFile"
+chmod 666 "$logFile"
 
-# Launch SwiftDialog in progress mode as user
-sudo -u "$loggedInUser" "$DIALOG" \
+# ==== Launch SwiftDialog as user ====
+launchctl asuser "$userID" sudo -u "$loggedInUser" "$DIALOG" \
   --title none \
   --bannerimage /usr/local/org/banner.jpeg \
   --bannerheight 200 \
@@ -41,29 +42,37 @@ sudo -u "$loggedInUser" "$DIALOG" \
   --button1text none \
   --blurscreen \
   --quitonlastline &
+
 DIALOG_PID=$!
 
+# ==== Run privileged actions (root context) ====
 sleep 4
 echo "progresstext: Configuring default dock..." >> "$logFile"
 echo "icon: SF=dock.arrow.down.rectangle" >> "$logFile"
-#sudo jamf policy -event 
+/usr/local/bin/jamf policy -event dock
+
 sleep 2
 echo "progresstext: Installing application..." >> "$logFile"
 echo "icon: SF=macbook" >> "$logFile"
-#sudo jamf policy -event 
+/usr/local/bin/jamf policy -event app_install
+
 sleep 2
 echo "progresstext: Verifying configuration..." >> "$logFile"
 echo "icon: SF=checklist" >> "$logFile"
-#sudo jamf policy -event 
+/usr/local/bin/jamf policy -event verify_setup
+
 sleep 2
 echo "progress: hide" >> "$logFile"
 echo "message: Configuration Complete!" >> "$logFile"
 echo "icon: SF=checkmark.seal,colour=green" >> "$logFile"
-#sudo jamf policy -event 
+
 sleep 2
-echo "message: + <br><br> Enjoy your Mac, and have a great $( date +'%A' )!" >> "$logFile"
+echo "message: + <br><br> Enjoy your Mac, and have a great $(date +'%A')!" >> "$logFile"
+
 sleep 4
 echo "quit:" >> "$logFile"
+
+exit 0
 
 
 
@@ -73,16 +82,24 @@ echo "quit:" >> "$logFile"
 <dict>
     <key>Label</key>
     <string>com.company.enrollment</string>
+
     <key>ProgramArguments</key>
     <array>
-        <string>/bin/bash</string>
-        <string>/usr/local/org/enrollment.sh</string>
+        <string>/usr/local/org/enrollment-wrapper.sh</string>
     </array>
+
     <key>RunAtLoad</key>
     <true/>
+
     <key>StandardOutPath</key>
-    <string>/tmp/enrollment_stdout.log</string>
+    <string>/var/log/enrollment_stdout.log</string>
     <key>StandardErrorPath</key>
-    <string>/tmp/enrollment_stderr.log</string>
+    <string>/var/log/enrollment_stderr.log</string>
+
+    <key>LaunchOnlyOnce</key>
+    <true/>
+
+    <key>LimitLoadToSessionType</key>
+    <string>Aqua</string>
 </dict>
 </plist>
