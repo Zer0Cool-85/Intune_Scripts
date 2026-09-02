c307d5f2d130a916d1b11622a7141499e7b63b2f
Dale <63794501+Zer0Cool-85@users.noreply.github.com>
2026-09-02 10:11:42 -0400

    Delete SystemSupportInfo directory

--- SystemSupportInfo/Launch-SystemSupportInfo.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-rem One-click launcher for the modular PowerShell version.
-start "" powershell.exe -NoLogo -NoProfile -STA -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0SystemSupportInfo.ps1"
-
-endlocal
-
